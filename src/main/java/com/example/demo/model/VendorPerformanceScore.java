9ce1c43047c467e1c8c7b178d3bc75606b0a0c7f
bala-amypo <bala@amypo.in>
2025-12-21 16:49:55 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorPerformanceScore.java
+++ src/main/java/com/example/demo/model/VendorPerformanceScore.java
@@ -3,13 +3,8 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(
-    name = "performance_scores",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = "delivery_evaluation_id")
-    }
-)
-public class PerformanceScore {
+@Table(name = "vendor_performance_scores")
+public class VendorPerformanceScore {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,33 +24,5 @@ public class PerformanceScore {
     )
     private DeliveryEvaluation deliveryEvaluation;
 
-    // getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public int getOverallScore() {
-        return overallScore;
-    }
-
-    public void setOverallScore(int overallScore) {
-        this.overallScore = overallScore;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public DeliveryEvaluation getDeliveryEvaluation() {
-        return deliveryEvaluation;
-    }
-
-    public void setDeliveryEvaluation(DeliveryEvaluation deliveryEvaluation) {
-        this.deliveryEvaluation = deliveryEvaluation;
-    }
+    // getters & setters
 }
